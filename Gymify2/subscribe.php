38505f7a743bcffe906fc857072bd3de3915185c
Franze Jr <franzejr@gmail.com>
2016-02-18 23:09:35 -0300

    Add second version of gymify landing page

---
+++ Gymify2/subscribe.php
@@ -0,0 +1,23 @@
+<?php
+$email_address = $_POST['sub'];
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+
+if( empty($errors))
+{
+	$to = 'info@trygymify.com';
+	$email_subject = "Gymify";
+	$email_body = "You have received a new Subscribe. ".
+	" Here are the details:\n Email: $email_address";
+
+	$headers = "From: $to\n";
+	$headers .= "Reply-To: $email_address";
+
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: thanks.html');
+}
+
+?>
